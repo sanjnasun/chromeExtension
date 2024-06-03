12e3fa86412136ed803c11f605c7b355ae24a1d6
Sanjna Sunil <sanjnasu@usc.edu>
2024-06-02 19:10:12 -0700

    new frontend files

---
+++ frontend/react-chrome-ext/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'; // Make sure to import the CSS file
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        Hello World
+      </div>
+    </div>
+  );
+}
+
+export default App;
