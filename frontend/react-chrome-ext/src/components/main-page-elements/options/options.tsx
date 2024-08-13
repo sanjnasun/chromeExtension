c5cc94feaada1a05d0b319af315f55760a49d76d
Sanjna Sunil <sanjnasu@usc.edu>
2024-08-13 15:37:51 -0700

    profile stuff

--- frontend/react-chrome-ext/src/components/main-page-elements/options/options.tsx
+++ frontend/react-chrome-ext/src/components/main-page-elements/options/options.tsx
@@ -1,9 +1,55 @@
-import React from 'react';
-import { Box } from '@mui/material';
+import React, {useState} from 'react';
+import { Box, Checkbox, FormControlLabel, FormControl, FormLabel, FormGroup} from '@mui/material';
 
 const Options = () => {
+    //a hook to create piece of state in the component 
+    const[options, setOptions] = useState<string[]>([])
+    
+    console.log({options})
+    const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const index = options.indexOf(event.target.value)
+      if(index === -1) {
+        setOptions([...options,event.target.value])
+      } else {
+        setOptions(options.filter((option) => option !== event.target.value))
+      }
+    }
+
     return (
-        <h5>Options </h5>
+        <div>
+        <h5>Options</h5>
+        <FormControl>
+                <FormLabel>News Preferences</FormLabel>
+                <FormGroup row>
+                    <FormControlLabel
+                        label="Sports"
+                        control={<Checkbox value='sports' checked={options.includes('sports')} onChange={handleOptionChange} />} 
+                    />
+                <FormControlLabel
+                    label="Entertainment"
+                    value = 'entertainment'
+                    control={<Checkbox checked={options.includes('entertainment')} onChange={handleOptionChange} />} 
+                    />
+                <FormControlLabel
+                    label="USC"
+                    value = 'usc'
+                    control={<Checkbox checked={options.includes('usc')} onChange={handleOptionChange} />}   
+                    />
+                 <FormControlLabel
+                    label="Politics"
+                    value = 'politics'
+                    control={<Checkbox checked={options.includes('politics')} onChange={handleOptionChange} />}   
+                    />
+                 <FormControlLabel
+                    label="Stock"
+                    value = 'stock'
+                    control={<Checkbox checked={options.includes('stock')} onChange={handleOptionChange} />}   
+                    />
+
+                </FormGroup>
+        </FormControl>
+    </div>
+
     );
 };
 
--- frontend/react-chrome-ext/src/components/main-page-elements/profile/profile.tsx
+++ frontend/react-chrome-ext/src/components/main-page-elements/profile/profile.tsx
@@ -1,9 +1,53 @@
 import React from 'react';
 import { Box } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 const Profile = () => {
+    const [age, setAge] = React.useState('');
+
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
+
     return (
-        <h5>Profile </h5>
+        <div>
+            <Typography sx={{ textAlign: 'center' }} variant="h1" component="h5">
+                Profile
+            </Typography>
+
+            <Typography variant="subtitle1" gutterBottom>
+                First name, Last Name
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                Email
+            </Typography>
+
+            <div>
+                <Typography variant="subtitle1" gutterBottom>
+                    Language
+                </Typography>
+
+                <FormControl fullWidth>
+                    <InputLabel id="language-select-label">Language</InputLabel>
+                    <Select
+                        labelId="language-select-label"
+                        id="language-select"
+                        value={age}
+                        label="Language"
+                        onChange={handleChange}
+                    >
+                        <MenuItem value={10}>English</MenuItem>
+                        <MenuItem value={20}>Spanish</MenuItem>
+                        <MenuItem value={30}>French</MenuItem>
+                    </Select>
+                </FormControl>
+            </div>
+        </div>
     );
 };
 
